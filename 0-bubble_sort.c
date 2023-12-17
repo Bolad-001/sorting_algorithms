fe65e4b3ed0e59a60e1ac9f715f1c03d3895cdbe
Bolad <adebayoabubakar44@gmail.com>
2023-12-17 04:54:50 -0800

    hot fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -29,6 +29,9 @@ void bubble_sort(int *array, size_t size)
 	size_t a, b;
 	int swapped = 0;
 
+	if (array == NULL || size == 0)
+		return;
+
 	for (a = 0; a < size - 1; a++)
 	{
 		for (b = 0; b < size - a - 1; b++)
