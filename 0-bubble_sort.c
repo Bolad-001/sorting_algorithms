a0fd7b8e641779ac9ae67db28f6f601472aa25e7
Bolad <adebayoabubakar44@gmail.com>
2023-12-17 04:43:23 -0800

    solution to task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * swap - function to swap an integer
+ * @i: first integer
+ * @j: second integer
+ *
+ * Return: Nothing
+ */
+
+void swap(int *i, int *j)
+{
+	int temp;
+
+	temp = *i;
+	*i = *j;
+	*j = temp;
+}
+
+/**
+  * bubble_sort - A function for bubble sorting.
+  * @array: The array to sort.
+  * @size: THe length of the aaray.
+  * Return: Nothing.
+  */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t a, b;
+	int swapped = 0;
+
+	for (a = 0; a < size - 1; a++)
+	{
+		for (b = 0; b < size - a - 1; b++)
+		{
+			if (array[b] > array[b + 1])
+			{
+				swap(&array[b], &array[b + 1]);
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+		if (!swapped)
+		return;
+	}
+
+	
+}
