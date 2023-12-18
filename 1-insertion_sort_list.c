825bd9a288c2e7bab01563c574ca6a6a7d064378
Bolad <adebayoabubakar44@gmail.com>
2023-12-18 11:25:10 -0800

    solution to sorting algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,61 @@
+#include "sort.h"
+
+/**
+ * swap_node - funtion that swap nodes in a
+ * double linked list
+ * @list: list to check
+ * @node1: first node
+ * @node2: second node
+ *
+ * Return: nothing
+ */
+
+void swap_node(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (node1->prev != NULL)
+		node1->prev->next = node2;
+	else
+		*list = node2;
+
+	if (node2->next != NULL)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
+}
+
+/**
+ * insertion_sort_list - function to sort a doubly lonked
+ * list using insertion sort
+ * @lsit: list to sort
+ *
+ * Return: nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *aux;
+
+	if (*list == NULL || (*list)->next == NULL)
+		return;
+
+ 	aux = (*list)->next;
+
+	while(aux != NULL)
+	{
+		temp = aux;
+		while(temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			swap_node(list, temp->prev, temp);
+			temp = temp->next;
+			print_list(*list);
+		}
+
+		aux = aux->next;
+
+	}
+
+
+}
