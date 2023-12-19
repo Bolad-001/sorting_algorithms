b912e35d4867206ebf29403453ab89e35624bdb9
Bolad <adebayoabubakar44@gmail.com>
2023-12-18 21:55:33 -0800

    fix error

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,6 +12,11 @@
 
 void swap_node(listint_t **list, listint_t *node1, listint_t *node2)
 {
+	listint_t *temp;
+
+	if (node1 == NULL || node2 == NULL || node1 == node2)
+		return;
+
 	if (node1->prev != NULL)
 		node1->prev->next = node2;
 	else
@@ -20,10 +25,9 @@ void swap_node(listint_t **list, listint_t *node1, listint_t *node2)
 	if (node2->next != NULL)
 		node2->next->prev = node1;
 
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-	node1->prev = node2;
-	node2->next = node1;
+	temp = node1->prev;
+	node1->prev = node2->prev;
+	node2->next = temp;
 }
 
 /**
